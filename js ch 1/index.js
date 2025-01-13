2e061daca115acc758c61e63f34be666f1475f92
SyedaManiaFaraz <syedamaniafaraz@gmail.com>
2025-01-13 22:58:01 +0500

    Add files via upload

---
+++ js ch 1/index.js
@@ -0,0 +1,31 @@
+// Question no 1
+alert("Welcome To My Website.")
+
+// Question No 2
+alert("Error! Please enter a valid passward.")
+
+// Question No 3
+alert("Welcome To JS Land..<br>Happy Coding!")
+
+// Question No 4
+alert("Welcome To JS Land")
+alert("Prevent this page from creating additional dialogs")
+
+// Question No 5
+alert ("Hello... I can run JS through my web browser's console")
+console.log("Hello... I can run JS through my web browser's console");
+
+// Question No 6
+HTML
+<h1>Welcome to the Alert Example</h1>
+<button class="alert-button" onclick="showAlert()">Click me for an Alert</button>
+JS
+function showAlert() {
+    alert("This is an alert message!");
+}
+
+// Question No 7
+<script>
+alert("Script in the Head section!");
+</script>
+
